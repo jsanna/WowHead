968f215ef9c8160fe39114deafe9b6b13e87720b
Johan <jsanna@qualixo.com>
2020-02-12 14:48:49 +0100

    help²

--- src/test/java/Autom4/wowhead/LardeurTest.java
+++ src/test/java/Autom4/wowhead/LardeurTest.java
@@ -11,39 +11,32 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class LardeurTest {
-	private String BROWSER=System.getProperty("browser");
+	private String BROWSER = System.getProperty("browser");
 	private WebDriver driver;
+
 	String Mob = "Lardeur";
 	String Item1 = "Chahuteur";
 	String Item2 = "Chausses";
 	String Item3 = "Pioche";
 	String Item4 = "Grève";
 	String Item5 = "Espauliers";
-	
+
 	String File1 = "Item1";
-	
-	public void chooseBrowser() {
-		if (BROWSER.equals("Firefox")){
-			WebDriver driver = new FirefoxDriver();
-			}
-			else if (BROWSER.equals("Chrome")) {
-			 WebDriver driver = new ChromeDriver();
-			}
-			else {
-			WebDriver driver = new InternetExplorerDriver();
-			}
 
-	}
-	
 	@Test
-	public void lardeurStuff() throws InterruptedException, FileNotFoundException {		
-PageAccueil pageAccueil = PageFactory.initElements(driver, PageAccueil.class);
-PageRecherchePNJ pageRecherchePNJ = pageAccueil.rechercheLardeur(driver, Mob);
-PagePNJ pagePNJ = pageRecherchePNJ.resultatLardeur(driver);
-PageItemLoot pageItemLoot = pagePNJ.premierItem(driver, Item1);
-//pageItemLoot.loadFile(File1);
-driver.navigate().back();
+	public void lardeurStuff() throws InterruptedException, FileNotFoundException {
+		if (BROWSER.equals("Firefox")) {
+			WebDriver driver = new FirefoxDriver();
+		} else if (BROWSER.equals("Chrome")) {
+			WebDriver driver = new ChromeDriver();
+		} else {
+			WebDriver driver = new InternetExplorerDriver();
+			PageAccueil pageAccueil = PageFactory.initElements(driver, PageAccueil.class);
+			PageRecherchePNJ pageRecherchePNJ = pageAccueil.rechercheLardeur(driver, Mob);
+			PagePNJ pagePNJ = pageRecherchePNJ.resultatLardeur(driver);
+			PageItemLoot pageItemLoot = pagePNJ.premierItem(driver, Item1);
+			// pageItemLoot.loadFile(File1);
+			driver.navigate().back();
+		}
 	}
-	
-
 }
